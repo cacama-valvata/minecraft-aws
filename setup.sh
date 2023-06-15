546bf8b406a5a2a7964400d028cc4ae064c900d1
Casey Colley <code@caseycolley.com>
2023-06-14 18:16:16 -0700

    spin up server, batteries included

---
+++ setup.sh
@@ -0,0 +1,10 @@
+terraform init
+terraform apply
+
+aws eks update-kubeconfig --name $(terraform output -raw cluster_name)
+kubectl get svc
+
+helm install my-minecraft -f helm-values.yaml minecraft-server-charts/minecraft
+
+CONN = kubectl get svc --namespace default my-minecraft-minecraft | grep my-minecraft-minecraft | awk '{ print $4 }'
+echo $CONN
